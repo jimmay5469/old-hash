615f4368af37caa91f0efe4a2c640050b8034635
jimmay5469 <jimmay5469@gmail.com>
2015-04-22 01:24:01 -0400

    Cleanup site flow

--- app/routes/github.js
+++ app/routes/github.js
@@ -2,14 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   session: Ember.inject.service(),
-  beforeModel: function() {
-    if(!this.get('session.isGithubAuthenticated')) {
-      this.replaceWith('index');
-    }
-  },
   model: function() {
+    if(!this.get('session.isGithubAuthenticated')) {
+      return;
+    }
     return this.get('session.githubUser').then(function(user) {
       return user.get('githubRepositories');
     });
+  },
+  actions: {
+    authenticateGithub: function() {
+      this.get('session').authenticateGithub().then(()=> {
+        this.refresh();
+      });
+    },
+    invalidateGithub: function() {
+      this.get('session').invalidateGithub();
+      window.location.reload();
+    }
   }
 });
