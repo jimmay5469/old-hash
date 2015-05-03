6e975bef8feb8354e54c00471e250328fd865e6c
jimmay5469 <jimmay5469@gmail.com>
2015-05-03 10:04:08 -0400

    Add github owner route

--- app/router.js
+++ app/router.js
@@ -8,5 +8,6 @@ var Router = Ember.Router.extend({
 export default Router.map(function() {
   this.route('github',function() {
     this.route('index', { path: '/' });
+    this.route('owner', { path: '/:ownerId' });
   });
 });
