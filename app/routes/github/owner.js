fea5f8076d75a317067d8ec8f43df48e0862487c
jimmay5469 <jimmay5469@gmail.com>
2015-05-04 10:50:54 -0400

    Use org model if owner is an org

--- app/routes/github/owner.js
+++ app/routes/github/owner.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.get('store').find('githubUser', params.ownerId);
+    return this.get('store').find('githubUser', params.ownerId).then((user)=> {
+      if(user.get('type') === 'Organization') {
+        return this.get('store').find('githubOrganization', params.ownerId);
+      }
+      return user;
+    });
   },
 });
